f01b2daf3c8062cebe73546d77bb1334a7a85803
JohnTheFeesh <johnthefeesh@protonmail.com>
2020-09-11 15:51:08 -0300

    Primeiro

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const connectDB = require('./config/db')
+const cors = require('cors')
+const path = require('path')
+
+const app = express()
+
+connectDB()
+
+app.use(express.json({ extended: false }))
+app.use(cors())
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('unimed/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'unimed', 'build', 'index.html'))
+    })
+}
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
---
+++ unimed/build/precache-manifest.c654f71c6ce2b35d93681e97f197e526.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37e4fbcf49afbd71a0a288046231da7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f7e2965704cd3e90327",
+    "url": "/static/css/main.36812627.chunk.css"
+  },
+  {
+    "revision": "33bf83f3f01b43e539e4",
+    "url": "/static/js/2.0c9a69be.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0c9a69be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f7e2965704cd3e90327",
+    "url": "/static/js/main.e5c9a268.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
